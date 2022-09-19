e409cdbeb78b140ec235348d736cd18fe7e1fc33
funnysuperst56 <joresu12@gmail.com>
2022-09-19 23:46:25 +0200

    Modified links

---
+++ js/controls.js
@@ -0,0 +1,8 @@
+const XBOX = document.getElementById("xbox");
+XBOX.onclick = () {
+        window.open("misc/i.xbox.html", "_blank");
+}
+
+#Xbox, #steam, #spotify, #pinterest, #reddit, #instagram{
+ cursor: pointer;
+}
