9825888e47a5046fe6ba4a8c035c86e6d6b9d0d3
funnysuperst56 <joresu12@gmail.com>
2023-03-20 06:30:34 +0100

    Update player music

--- js/controls.js
+++ js/controls.js
@@ -1,8 +1,49 @@
-const XBOX = document.getElementById("xbox");
-XBOX.onclick = () {
-        window.open("misc/i.xbox.html", "_blank");
+const musicPlayer = document.querySelector('.music-player');
+const audio = document.querySelector('#audio');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+const lineContainer = document.querySelector('.line-container');
+const line = document.querySelector('.line');
+const prevButton = document.querySelector('#previous');
+const playPauseButton = document.querySelector('#play-pause');
+const nextButton = document.querySelector('#next');
+
+//Songs titles
+const songs = ['Symbolism', 'Heroes Tonight', 'Sky High']
+
+let songIdx = 0;
+
+loadSong(songs[songIdx])
+
+//Update name, audio, duration and image
+function loadSong(song){
+        title.innerText = song;
+        audio.src = `Songs/${song}.mp3`;
+        cover.src = `Images/${song}.jpg`;
 }
 
-#Xbox, #steam, etc{
- cursor: pointer;
+function playSong(){
+        musicPlayer.classList.add('play')
+        playPauseButton.querySelector('i.fas').classList.remove('fa-play')
+        playPauseButton.querySelector('i.fas').classList.add('fa-pause')
+
+        audio.play()
 }
+
+function pauseSong(){
+        musicPlayer.classList.remove('play')
+        playPauseButton.querySelector('i.fas').classList.add('fa-play')
+        playPauseButton.querySelector('i.fas').classList.remove('fa-pause')
+
+        audio.pause()
+}
+
+playPauseButton.addEventListener('click', () =>{
+        const isPlaying = musicPlayer.classList.contains('play');
+        if(isPlaying){
+              pauseSong()  
+        }else{
+              playSong() 
+        }
+})
+
